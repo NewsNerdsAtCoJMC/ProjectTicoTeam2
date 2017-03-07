65c1fd1440769a9ae2955e696d5d9c94ce98f6e1
vincentpena <vcp6@nau.edu>
2017-03-07 11:22:32 -0700

    Added pool to amenities

--- studentsonly/apartments/models.py
+++ studentsonly/apartments/models.py
@@ -4,7 +4,8 @@ class Building(models.Model):
     name = models.CharField(max_length=255)
 
 class Amenities(models.Model):
-	
+    pool = models.BooleanField()
+
 class Apartment(models.Model):
     building = models.ForeignKey(Building)
     amenities = models.ManyToManyField(Amenities)
