428c868329ba8a7618bb544690f2d8cf4bd22cb4
vincentpena <vcp6@nau.edu>
2017-03-09 10:32:40 -0700

    added people stuff to models.py

--- studentsonly/apartments/models.py
+++ studentsonly/apartments/models.py
@@ -1,34 +1,23 @@
 from django.db import models
 
+class People(models.Model):
+    nuid = models.CharField(max_length=8)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    age = models.IntegerField()
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    year = models.CharField()
+    major = models.CharField()
+    
 class Building(models.Model):
-	name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
 class Amenities(models.Model):
-	pool = models.BooleanField()
-	pets = models.BooleanField()
-	cable = models.BooleanField()
-	wifi = models.BooleanField()
-	electric = models.BooleanField()
-	smoking = models.BooleanField()
-	electric = models.BooleanField()
-	gym = models.BooleanField()
-	reservedParking = models.BooleanField()
-	coveredParking = models.BooleanField()
+    pool = models.BooleanField()
 
 class Apartment(models.Model):
-	rating = models.ForeignKey(Rating)
-	building = models.ForeignKey(Building)
-	amenities = models.ManyToManyField(Amenities)
-	description = models.TextField()
-	distanceToCity = models.BooleanField()
-	distanceToEast = models.BooleanField()
-	price = models.IntegerField()
-	leasingOptions = models.CharField()
-	bedrooms = models.IntegerField() #consider changing type
-
-class Rating(models.Model):
-	apartment = models.ForeignKey(Apartment)
-	text = models.CharField(maxLength=1000)
-	hospitality = models.IntegerField() #add validators to restrict max value
-	maintenance = models.IntegerField()
-	safety_security = models.IntegerField()
+    building = models.ForeignKey(Building)
+    amenities = models.ManyToManyField(Amenities)
+    description = models.TextField()
+    bedrooms = models.IntegerField()
