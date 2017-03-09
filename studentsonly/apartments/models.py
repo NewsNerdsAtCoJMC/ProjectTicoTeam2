127d73d57773e02fbfc1594f9e603910b721e3af
aphegarty <aphegarty@huskers.unl.edu>
2017-03-09 12:32:25 -0600

    all models

--- studentsonly/apartments/models.py
+++ studentsonly/apartments/models.py
@@ -1,7 +1,37 @@
 from django.db import models
+#from django.core.validators import MaxValueValidator
+
+class People(models.Model):
+	email = models.CharField(max_length=255)
+	reviews = models.ManyToManyField(Reviews)
+	apartmentPreferences = models.ForeignKey(Apartment) #search
+	profile = models.ForeignKey(Profile)
+	phone = models.CharField(max_length=20)
+	birthday = models.DateField()
+	firstName = models.CharField(max_length=255)
+	lastName = models.CharField(max_length=255)
+	year = models.CharField(max_length=255)
+	major = models.CharField(max_length=255)
+
+class Profile(models.Model):
+	noise = models.PositiveIntegerField() #5: ok with loud music, 1: need silence
+	social_academic = models.PositiveIntegerField()#5: ok with loud music, 1: need silence
+	temp = models.PositiveIntegerField()#5 warm, 1: cool
+	cleanliness = models.PositiveIntegerField()#5 very clean 1 total mess
+	visitors = models.PositiveIntegerField()#5 like them 1 hate them
+	drinking = models.PositiveIntegerField()# 5 i drink a lot - 1: never drink
+	smoking = models.CharField()#consider changing type #yes. #no, but okay. #no, not okay.
+	sleepTime = models.CharField() #early riser #night owl
+	durationOfStayMonths = models.PositiveIntegerField()
+	interests = models.CharField(max_length=1000)
 
 class Building(models.Model):
 	name = models.CharField(max_length=255)
+	address = models.CharField(max_length=255)
+	contactInfo = models.CharField(max_length=255)
+	available = models.BooleanField()
+	def __str__(self):
+		return self.name
 
 class Amenities(models.Model):
 	pool = models.BooleanField()
@@ -16,7 +46,8 @@ class Amenities(models.Model):
 	coveredParking = models.BooleanField()
 
 class Apartment(models.Model):
-	rating = models.ForeignKey(Rating)
+	nickname = models.CharField(max_length=255)
+	rating = models.PositiveIntegerField() #derived from reviews
 	building = models.ForeignKey(Building)
 	amenities = models.ManyToManyField(Amenities)
 	description = models.TextField()
@@ -24,11 +55,16 @@ class Apartment(models.Model):
 	distanceToEast = models.BooleanField()
 	price = models.IntegerField()
 	leasingOptions = models.CharField()
-	bedrooms = models.IntegerField() #consider changing type
+	bedrooms = models.CharField()
+	def __str__(self):
+		return self.nickname
 
-class Rating(models.Model):
-	apartment = models.ForeignKey(Apartment)
-	text = models.CharField(maxLength=1000)
-	hospitality = models.IntegerField() #add validators to restrict max value
-	maintenance = models.IntegerField()
-	safety_security = models.IntegerField()
+class Reviews(models.Model):
+	apartment = models.ForeignKey(Building)
+	comment = models.CharField(max_length=1000)
+	hospitality = models.PositiveIntegerField() 
+	maintenance = models.PositiveIntegerField()
+	safety_security = models.PositiveIntegerField()
+	overall = models.PositiveIntegerField()
+	def __str__(self):
+		return "%s" % self.overall
