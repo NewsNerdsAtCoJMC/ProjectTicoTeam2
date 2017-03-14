51456416e5a11e0894362f194c048705fa20f8a3
aphegarty <aphegarty@huskers.unl.edu>
2017-03-14 12:13:05 -0500

    Added admin, fixed stupid changes

--- studentsonly/apartments/admin.py
+++ studentsonly/apartments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, Building, Amenities, Apartment, People, Reviews
+
+admin.site.register(Profile)
+admin.site.register(Building)
+admin.site.register(Amenities)
+admin.site.register(Apartment)
+admin.site.register(People)
+admin.site.register(Reviews)
--- studentsonly/apartments/models.py
+++ studentsonly/apartments/models.py
@@ -1,69 +1,72 @@
 from django.db import models
 
-class People(models.Model):
-	email = models.CharField(max_length=255)
-	reviews = models.ManyToManyField(Reviews)
-	apartmentPreferences = models.ForeignKey(Apartment) #search
-	profile = models.ForeignKey(Profile)
-	phone = models.CharField(max_length=20)
-	birthday = models.DateField()
-	firstName = models.CharField(max_length=255)
-	lastName = models.CharField(max_length=255)
-	year = models.CharField(max_length=255)
-	major = models.CharField(max_length=255)
-
 class Profile(models.Model):
-	noise = models.PositiveIntegerField() #5: ok with loud music, 1: need silence
-	social_academic = models.PositiveIntegerField()#5: ok with loud music, 1: need silence
-	temp = models.PositiveIntegerField()#5 warm, 1: cool
-	cleanliness = models.PositiveIntegerField()#5 very clean 1 total mess
-	visitors = models.PositiveIntegerField()#5 like them 1 hate them
-	drinking = models.PositiveIntegerField()# 5 i drink a lot - 1: never drink
-	smoking = models.CharField()#consider changing type #yes. #no, but okay. #no, not okay.
-	sleepTime = models.CharField() #early riser #night owl
-	durationOfStayMonths = models.PositiveIntegerField()
-	interests = models.CharField(max_length=1000)
+    noise = models.PositiveIntegerField() #5: ok with loud music, 1: need silence
+    social_academic = models.PositiveIntegerField()#5: ok with loud music, 1: need silence
+    temp = models.PositiveIntegerField()#5 warm, 1: cool
+    cleanliness = models.PositiveIntegerField()#5 very clean 1 total mess
+    visitors = models.PositiveIntegerField()#5 like them 1 hate them
+    drinking = models.PositiveIntegerField()# 5 i drink a lot - 1: never drink
+    smoking = models.CharField(max_length=255)#todo:restrict options #yes. #no, but okay. #no, not okay.
+    sleepTime = models.CharField(max_length=255)#todo:restrict options #early riser #night owl
+    durationOfStayMonths = models.PositiveIntegerField()
+    interests = models.CharField(max_length=1000)
 
 class Building(models.Model):
-	name = models.CharField(max_length=255)
-	address = models.CharField(max_length=255)
-	contactInfo = models.CharField(max_length=255)
-	distanceToCity = models.BooleanField()
-	distanceToEast = models.BooleanField()
-	available = models.BooleanField()
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    contactInfo = models.CharField(max_length=255)
+    distanceToCity = models.FloatField()
+    distanceToEast = models.FloatField()
+    available = models.BooleanField()
+    def __str__(self):
+        return self.name
 
 class Amenities(models.Model):
-	pool = models.BooleanField()
-	pets = models.BooleanField()
-	cable = models.BooleanField()
-	wifi = models.BooleanField()
-	electric = models.BooleanField()
-	smoking = models.BooleanField()
-	electric = models.BooleanField()
-	gym = models.BooleanField()
-	reservedParking = models.BooleanField()
-	coveredParking = models.BooleanField()
+    pool = models.BooleanField()
+    pets = models.BooleanField()
+    cable = models.BooleanField()
+    wifi = models.BooleanField()
+    electric = models.BooleanField()
+    smoking = models.BooleanField()
+    electric = models.BooleanField()
+    gym = models.BooleanField()
+    reservedParking = models.BooleanField()
+    coveredParking = models.BooleanField()
 
 class Apartment(models.Model):
-	nickname = models.CharField(max_length=255)
-	rating = models.PositiveIntegerField() #derived from reviews
-	building = models.ForeignKey(Building)
-	amenities = models.ManyToManyField(Amenities)
-	description = models.TextField()
-	price = models.IntegerField()
-	leasingOptions = models.CharField()
-	bedrooms = models.CharField()
-	def __str__(self):
-		return self.nickname
+    nickname = models.CharField(max_length=255)
+    rating = models.PositiveIntegerField() #derived from reviews
+    building = models.ForeignKey(Building)
+    amenities = models.ManyToManyField(Amenities)
+    description = models.TextField()
+    price = models.IntegerField()
+    leasingOptions = models.CharField(max_length=255)
+    bedrooms = models.PositiveIntegerField()
+    def __str__(self):
+        return self.nickname
 
+
+class People(models.Model):
+    email = models.CharField(max_length=255)
+    apartmentPreferences = models.ManyToManyField(Apartment) #search
+    profile = models.ForeignKey(Profile)
+    phone = models.CharField(max_length=20)
+    birthday = models.DateField()
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
+    major = models.CharField(max_length=255)
+
+    
 class Reviews(models.Model):
-	apartment = models.ForeignKey(Building)
-	comment = models.CharField(max_length=1000)
-	hospitality = models.PositiveIntegerField() 
-	maintenance = models.PositiveIntegerField()
-	safety_security = models.PositiveIntegerField()
-	overall = models.PositiveIntegerField()
-	def __str__(self):
-		return "%s" % self.overall
+    apartment = models.ForeignKey(Building)
+    author = models.ForeignKey(People)
+    comment = models.TextField()
+    hospitality = models.PositiveIntegerField() 
+    maintenance = models.PositiveIntegerField()
+    safety_security = models.PositiveIntegerField()
+    overall = models.PositiveIntegerField()
+    def __str__(self):
+        return "%s" % self.overall
+        
