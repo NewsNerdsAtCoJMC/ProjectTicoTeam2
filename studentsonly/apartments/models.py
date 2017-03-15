1167612e497bbf0f45a2dbef83af5223f674c21e
aphegarty <aphegarty@huskers.unl.edu>
2017-03-15 15:38:11 -0500

    Moved stuff from apt to building
    rating and amenities

--- studentsonly/apartments/models.py
+++ studentsonly/apartments/models.py
@@ -12,16 +12,6 @@ class Profile(models.Model):
     durationOfStayMonths = models.PositiveIntegerField()
     interests = models.CharField(max_length=1000)
 
-class Building(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    contactInfo = models.CharField(max_length=255)
-    distanceToCity = models.FloatField()
-    distanceToEast = models.FloatField()
-    available = models.BooleanField()
-    def __str__(self):
-        return self.name
-
 class Amenities(models.Model):
     pool = models.BooleanField()
     pets = models.BooleanField()
@@ -33,19 +23,28 @@ class Amenities(models.Model):
     gym = models.BooleanField()
     reservedParking = models.BooleanField()
     coveredParking = models.BooleanField()
+    
+class Building(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    contactInfo = models.CharField(max_length=255)
+    rating = models.FloatField(default=0.0) #derived from reviews
+    amenities = models.ManyToManyField(Amenities)
+    distanceToCity = models.FloatField()
+    distanceToEast = models.FloatField()
+    available = models.BooleanField()
+    def __str__(self):
+        return self.name
 
 class Apartment(models.Model):
     nickname = models.CharField(max_length=255)
-    rating = models.PositiveIntegerField() #derived from reviews
     building = models.ForeignKey(Building)
-    amenities = models.ManyToManyField(Amenities)
     description = models.TextField()
     price = models.IntegerField()
     leasingOptions = models.CharField(max_length=255)
     bedrooms = models.PositiveIntegerField()
     def __str__(self):
         return self.nickname
-
 
 class People(models.Model):
     email = models.CharField(max_length=255)
@@ -57,7 +56,6 @@ class People(models.Model):
     lastName = models.CharField(max_length=255)
     year = models.CharField(max_length=255)
     major = models.CharField(max_length=255)
-
     
 class Reviews(models.Model):
     apartment = models.ForeignKey(Building)
