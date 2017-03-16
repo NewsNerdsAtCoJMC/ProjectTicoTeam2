bb32d94f511dc2a04b715a46fe0a1c1891ea639f
aphegarty <aphegarty@huskers.unl.edu>
2017-03-16 11:21:48 -0500

    Half a view

--- studentsonly/apartments/views.py
+++ studentsonly/apartments/views.py
@@ -3,8 +3,20 @@ from django.http import HttpResponse
 from django.db.models import Count
 from enrollment.models import People, Profile, Building, Amenities, Apartment, Reviews
 
-def index(request):
+def index(request): #List Buildings
     closeList = Building.objects.all().order_by('distanceToCity')[:10]
     context = {'closeList':closeList} #creates a dictionary
     return render(request, 'enrollment/index.html', context)
+    
+#a page for a specific building
+def building(request, building_slug):
+    building = Building.objects.get(name=building_slug)
+    
+    
+#list apartments in a given building
+def apartments(request, 
+
+
+
+
 
