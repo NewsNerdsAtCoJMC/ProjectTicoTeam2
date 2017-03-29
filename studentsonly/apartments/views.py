9c754414628cfd8bfa90e31bbadb33371d51afa3
aphegarty <aphegarty@huskers.unl.edu>
2017-03-29 15:14:30 -0500

    :-1:
    ?

--- studentsonly/apartments/views.py
+++ studentsonly/apartments/views.py
@@ -8,7 +8,6 @@ def index(request): #List Apartments
     context = {'aptList':aptList} #creates a dictionary
     return render(request, 'apartments/index.html', context)
     
-    
 #a page for a specific building and list all available apartments and all reviews
 def building(request, building_slug): #Building detail
     building = Building.objects.get(name_slug=building_slug)
@@ -17,6 +16,8 @@ def building(request, building_slug): #Building detail
     score = Reviews.objects.filter(building__name_slug=building_slug).aggregate(Avg('rating'))#annotate or aggrogate
     context = {'building':building, 'allReviews':allReviews, 'allApts':allApts, 'score':score}
 
+    
+    
 
     
 
