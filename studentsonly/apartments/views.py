beca7e055236affc6b5cb84472f541154c179826
aphegarty <aphegarty@huskers.unl.edu>
2017-03-29 15:07:26 -0500

    add building view, modify index

--- studentsonly/apartments/views.py
+++ studentsonly/apartments/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Count
-from enrollment.models import People, Profile, Building, Amenities, Apartment, Reviews
+from apartments.models import People, Profile, Building, Amenities, Apartment, Reviews
 
-def index(request): #List Buildings
-    closeList = Building.objects.all().order_by('distanceToCity')
-    context = {'closeList':closeList} #creates a dictionary
-    return render(request, 'enrollment/index.html', context)
+def index(request): #List Apartments
+    aptList = Apartment.objects.all()#LATER (order by date-time published)
+    context = {'aptList':aptList} #creates a dictionary
+    return render(request, 'apartments/index.html', context)
     
-#a page for a specific building and list all available apartments
-def building(request, building_slug):
+    
+#a page for a specific building and list all available apartments and all reviews
+def building(request, building_slug): #Building detail
     building = Building.objects.get(name_slug=building_slug)
+    allReviews = Reviews.objects.filter(building__name_slug=building_slug)
     allApts = Apartment.objects.filter(building__name_slug=building_slug) 
-    score = Reviews.objects.filter(building__name_slug=building_slug).#annotate or aggrogate
-    
+    score = Reviews.objects.filter(building__name_slug=building_slug).aggregate(Avg('rating'))#annotate or aggrogate
+    context = {'building':building, 'allReviews':allReviews, 'allApts':allApts, 'score':score}
+
+
     
 
 
