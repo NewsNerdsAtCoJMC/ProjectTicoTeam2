05a13e5adf30997006792177d636ab48cdd896a9
vincentpena <vcp6@nau.edu>
2017-04-25 11:17:46 -0700

    killin it with all the changes that matt made in templates for rating, etc.

--- studentsonly/apartments/views.py
+++ studentsonly/apartments/views.py
@@ -18,9 +18,9 @@ def buildingList(request):
 #a page for a specific building and list all available apartments and all reviews
 def building(request, building_slug): #Building detail
     building = Building.objects.get(name_slug=building_slug)
-    allReviews = Reviews.objects.filter(building__name_slug=building_slug)
-    allApts = Apartment.objects.filter(building__name_slug=building_slug) 
-    score = Reviews.objects.filter(building__name_slug=building_slug).aggregate(Avg('overall'))#annotate or aggrogate
+    allReviews = Reviews.objects.filter(building=building)
+    allApts = Apartment.objects.filter(building=building) 
+    score = Reviews.objects.filter(building=building).aggregate(Avg('overall'))#annotate or aggrogate
     context = {'building':building, 'allReviews':allReviews, 'allApts':allApts, 'score':score}
     return render(request, 'apartments/building.html', context)
 
@@ -51,7 +51,7 @@ def postreview(request):
     else:
         form = ReviewForm()
         context = {'form': form}
-    return render(request, 'apartments/reviews.html', context)
+    return render(request, 'apartments/review.html', context)
     
     
 
